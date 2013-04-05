5fe0473aba987984464a2960b87b7c105a968f15
lai-c <lai-c@klab.com>
2013-04-05 17:06:34 +0900

    add junit test

---
+++ src/HelloWorldTest.java
@@ -0,0 +1,7 @@
+public class HelloWorldTest extends junit.framework.TestCase {
+    public void testNothing() {
+    }       
+    public void testWillAlwaysFail() {
+        fail("An error message");
+    }
+}
